fb0151cef9c28a667218480e7436248399d9a272
Alonso <l24200197@pachuca.tecnm.mx>
2026-09-10 10:35:39 -0600

    Corrige API_URL

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,4 @@
 const API_URL = "https://12aplicacioneswebapp1.vercel.app/api/chat";
-
 const form = document.getElementById("chatForm");
 const input = document.getElementById("messageInput");
 const messages = document.getElementById("messages");
